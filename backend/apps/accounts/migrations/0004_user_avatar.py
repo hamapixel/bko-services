889abc85cd229b75bb1ca68624bb2b1e06ab493d
Hama A Traore <hamatraore519@gmail.com>
2026-09-24 17:14:23 +0100

    feat(accounts): migrate user avatar field

---
+++ backend/apps/accounts/migrations/0004_user_avatar.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("accounts", "0003_passwordresetcode"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="user",
+            name="avatar",
+            field=models.FileField(blank=True, upload_to="avatars/"),
+        ),
+    ]
